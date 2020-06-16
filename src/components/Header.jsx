89315344694da07734216ddb4aeb6b0ecfc0c4fd
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2020-06-16 11:20:31 -0400

    fixed responsiveness for project page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Header.css'
-
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-       
-        
-        
       <nav>
-        
-       
-      <Link to="/">Home</Link>
+        <Link to="/">Home</Link>
         <span className="divider"></span>
         <Link to="/about-me">About Me</Link>
         <span className="divider"></span>
@@ -21,8 +15,6 @@ function Header() {
         {/* <Link to="/skills">Skills</Link>
         <span className="divider"></span> */}
         <Link to="/contact">Contact</Link>
-      
-        
       </nav>
     </div>
   );
