c993181f1913161c975f31bf1a9b180434ae8301
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2020-06-19 02:51:31 -0400

    contact form functions

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,102 +5,79 @@ import "./Contact.css";
 import Coffee from "../assets/coffee.jpg";
 import Beans from "../assets/beans.jpg";
 
-class Contact extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: "",
-    };
-  }
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
-    console.log(event.target.value);
-  };
+function Contact() {
+  return (
+    <div id="Contact">
+      <div className="email-form">
+        <form
+          action="mailto:roxanadeng@gmail.com"
+          method="post"
+          encType="text/plain"
+        >
+          <label>
+            <p>Name</p>
+          </label>
+          <span className="down"></span>
+          <br />
+          <br />
+          <input type="text" name="Name"></input>
 
-  handleSubmit = (event) => {
-    alert("A name was submitted: " + this.state.value);
-    event.preventDefault();
-  };
-
-  render() {
-    return (
-      <div id="Contact">
-        <div className="email-form">
-          <form
-            action="mailto:roxanadeng@gmail.com"
-            method="post"
-            onSubmit={this.handleSubmit}
-            encType="text/plain"
-          >
-            <label>
-              <p>Name</p>
-            </label>
-            <span className="down"></span>
-            <br />
-            <br />
-            <input
-              type="text"
-              name="Name"
-              value={this.state.value}
-              onChange={this.handleChange}
-            ></input>{" "}
-            <br />
-            <br />
-            <label>
-              <p>Email</p>
-            </label>
-            <span className="down"></span>
-            <br />
-            <br />
-            <input type="email" name="Email"></input>
-            <br />
-            <br />
-            <label>
-              <p>Message</p>
-            </label>
-            <span className="down"></span>
-            <br />
-            <br />
-            <textarea name="Message" rows="10" cols="30"></textarea>
-            <br />
-            <button className="submit">Send</button>
-          </form>
-        </div>
-        <div className="contact-container">
-          <h2>Contact</h2>
-          <button>
-            <Link to={resume} target="_blank">
-              Resume
-            </Link>
-          </button>
+          <br />
+          <label>
+            <p>Email</p>
+          </label>
+          <span className="down"></span>
+          <br />
+          <br />
+          <input type="email" name="Email"></input>
           <br />
 
-          <button>
-            <a
-              href="https://github.com/dengroxana"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </button>
+          <label>
+            <p>Message</p>
+          </label>
+          <span className="down"></span>
           <br />
-          <button>
-            <a
-              href="https://www.linkedin.com/in/roxanadeng/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>
-          </button>
-        </div>
-        <div className="contact-img">
-          <img src={Coffee} alt="" />
-          <img src={Beans} alt="" />
-        </div>
+          <br />
+          <textarea name="Message" rows="10" cols="30"></textarea>
+          <br />
+          <button className="submit-btn">Send</button>
+        </form>
       </div>
-    );
-  }
+      <div className="contact-container">
+        <h2>Contact</h2>
+        <button>
+          <Link to={resume} target="_blank">
+            Resume
+          </Link>
+        </button>
+        <br />
+
+        <button>
+          <a
+            href="https://github.com/dengroxana"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </button>
+        <br />
+        <button>
+          <a
+            href="https://www.linkedin.com/in/roxanadeng/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </button>
+      </div>
+      <div className="contact-img">
+        <img src={Coffee} alt="" />
+        <img src={Beans} alt="" />
+      </div>
+    </div>
+  );
 }
+
 export default Contact;
